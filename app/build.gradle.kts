b0dad25e0f3b1d7ccfb5ab3d7bbb7d801c60109b
mcmoict <mcmoict@gmail.com>
2026-09-23 19:41:16 +0900

    README.md 파일 현행화, 마이너 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.innoshiftconsult.pocketlock"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.0"
+        versionCode = 22
+        versionName = "1.0.1"
     }
 
     signingConfigs {
